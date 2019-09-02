28d84acac44aa665ae05ca675ab7720de8b5b800
AmrDeveloper <amr96@programmer.net>
2019-09-02 23:32:19 +0200

    Create TaskViewModel

---
+++ app/src/main/java/com/amrdeveloper/tasktimer/TaskViewModel.java
@@ -0,0 +1,41 @@
+package com.amrdeveloper.tasktimer;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TaskViewModel extends AndroidViewModel{
+
+    private TaskRepository repository;
+    private LiveData<List<Task>> allTasks;
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+        repository = new TaskRepository(application);
+        allTasks = repository.getAllTasks();
+    }
+
+    public void insert(Task task){
+        repository.insert(task);
+    }
+
+    public void update(Task task){
+        repository.update(task);
+    }
+
+    public void delete(Task task){
+        repository.delete(task);
+    }
+
+    public void deleteAll(){
+        repository.deleteAll();
+    }
+
+    public LiveData<List<Task>> getAllTasks() {
+        return allTasks;
+    }
+}
